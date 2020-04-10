5157b966d0018c6d874d36ef9d483569f32d7559
singhadarsh001 <49779295+singhadarsh001@users.noreply.github.com>
2020-04-10 23:07:03 +0530

    Create destination.blade (1).php

---
+++ destination.blade (1).php
@@ -0,0 +1,272 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<title> Home</title>
+	</head>
+
+<body>
+								@yield('header')          <!-- extends the header code here -->
+<div class="headline">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="headline-text">
+                        <h3>WelCome to BIHAR</h3>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><br><br>
+
+     <div class="destination">
+        <div class="container">
+            <div class="row content-center">
+                <div class="col-lg-6">
+                    <div class="title-center">
+                        <h3>Destination</h3>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/1.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Patna <a href="/places/1">  06 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/2.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Bodh Gaya <a href="/places/2">  09 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/3.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Aurangabad <a href="/places/3">  02 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/4.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Rajgir <a href="/places/4">  09 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/5.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Nalanda  <a href="/places/5">  06 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="destination">
+                            <img src="img/destination/6.png" alt="" height="250px" width="300px">
+                        </div>
+                        <div class="content">
+                            <p class="align-center">Sasaram <a href="/places/6">  03 Places</a> </p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                            
+
+
+ <!--                        ***************************   Popular places ******************** 
+  -->                       
+
+  <div class="places_area">
+        <div class="container">
+            <div class="rowcontent-center">
+                <div class="col-lg-6">
+                    <div class="title-center">
+                        <h3>Popular Places</h3>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/1.png" alt="" height="250px" width="300px">
+                   
+                            <a href="/1"><h3>Patna</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/2.png" alt="" height="250px" width="300px">
+                            <a href="/2"><h3>Bodh Gaya</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/3.png" alt="" height="250px" width="300px">
+                            
+    
+                            <a href="/3"><h3>Gaya</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/4.png" alt="" height="250px" width="300px">
+                            <a href="/4"><h3>Rajgir</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/5.png" alt="" height="250px" width="300px">
+                            <a href="/5"><h3>Nalanda</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="place">
+                            <img src="img/place/6.png" alt="" height="250px" width="300px">
+                            <a href="/6"><h3>Vaishali</h3></a>
+                            <p>Bihar</p>
+                            <div class="days">
+                                <div class="days">
+                                    <i class="clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="btn-center">
+                        <a class="btn4" href="/more">More Places</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!-- ***********************************Places**************************************** -->
+
+<!-- ***********************************Recent Trips********************************** -->
+       
+    <div class="recent_trip_area">
+        <div class="container">
+            <div class="rowcontent-center">
+                <div class="col-lg-6">
+                    <div class="title-center">
+                        <h3>Most visited Places</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                            <img src="img/trip/1.png" alt="" height="250px" width="300px">
+                        </div>
+                            <div class="date">
+                                <span>38K Online bookings</span>
+                            </div>
+                         <a href="/1">
+                        
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                            <img src="img/trip/2.png" alt="" height="250px" width="300px">
+                        </div>
+                            <div class="date">
+                                <span>45K Online bookings</span>
+                            </div>
+                             <a href="/2">
+                        
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                            <img src="img/trip/3.png" alt="" height="250px" width="300px">
+                        </div>
+                            <div class="date">
+                                <span>87K Online bookings</span>
+                            </div>
+                            <a href="/3">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ***********************************Recent Trips********************************** -->
+
+
+								@yield('footer')          <!-- extends the footer code here -->                                 
+
+</body>
+</html>
