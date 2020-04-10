a7a6c98d3315f79bd5ce7e2d85b313d7237a96ec
singhadarsh001 <49779295+singhadarsh001@users.noreply.github.com>
2020-04-10 22:55:51 +0530

    Create singleDestination.blade.php

---
+++ singleDestination.blade.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<title> Home</title>
+	</head>
+
+<body>
+                                @yeild('header')          <!-- extends the header code here -->
+   
+
+    <div class="destination">
+        <div class="dtext-center">
+            <h3>Bodhgaya</h3>
+            <p>The Land of Enlightenment of the Budhha</p>
+        </div>
+    </div>
+
+    <div class="destination_details_info">
+        <div class="container">
+                <div class="col-lg-8 col-md-9">
+                   
+                        <h3>Description</h3>
+                        <p>Bodh Gaya is a religious site and place of pilgrimage associated with the Mahabodhi Temple Complex in Gaya district in the Indian state of Bihar. It is famous as it is the place where Gautama Buddha is said to have attained Enlightenment (Pali: bodhi) under what became known as the Bodhi Tree.For Buddhists, Bodh Gaya is the most important of the main four pilgrimage sites related to the life of Gautama Buddha,the other three being Kushinagar, Lumbini, and Sarnath. In 2002, Mahabodhi Temple, located in Bodh Gaya, became a UNESCO World Heritage Site.The history of Bodh Gaya is documented by many inscriptions and pilgrimage accounts. Foremost among these are the accounts of the Chinese pilgrims Faxian in the 5th century and Xuanzang in the 7th century.</p>
+                        <h2>What to Visit</h2>
+                        <p>Mahabodhi Temple<p>
+                            <p>Bodhi Tree<p>
+                                <p>Chinease Temple<p>
+                                     <p>Sujata kutir<p>
+                                         <p>Ratnagarh<p>
+                                    
+                     <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="splace">
+                        <div class="thumb">
+                            <img src="img/place/10.png" alt="" height="250px" width="300px">
+                            
+                        </div>            
+                        
+                    </div>
+                </div>
+         </div>
+
+   
+
+    <div class="places">
+        <div class="container">
+            <div class="row-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center mb_70">
+                        <h3>More Places</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_place">
+                        <div class="thumb">
+                            <img src="img/place/1.png" alt="" height="250px" width="300px">
+                            
+                        </div>
+                        <div class="place_info">
+                            <a href="#"><h3>Mahavir Mandir</h3></a>
+                            <p></p>
+                            <div class="rating_days d-flex justify-content-between">
+                                <span class="d-flex justify-content-center align-items-center">
+                                     <i class="fa fa-star"></i> 
+                                     <i class="fa fa-star"></i> 
+                                     <i class="fa fa-star"></i> 
+                                     <i class="fa fa-star"></i> 
+                                     <i class="fa fa-star"></i>
+                                     <a href="#">(20 Review)</a>
+                                </span>
+                                <div class="days">
+                                    <i class="fa fa-clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_place">
+                        <div class="thumb">
+                            <img src="img/place/2.png" alt="" height="250px" width="300px">
+                        
+                        </div>
+                        <div class="place_info">
+                            <a href="#"><h3>Mahabodhi Temple</h3></a>
+                            
+                            <div class="rating_days d-flex justify-content-between">
+                        
+                                <div class="days">
+                                    <i class="fa fa-clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_place">
+                        <div class="thumb">
+                            <img src="img/place/3.png" alt="" height="250px" width="300px">
+                    
+                        </div>
+                        <div class="place_info">
+                            <a href="#"><h3>Gaya Vishnupad Temple</h3></a>
+                            <div class="rating_days d-flex justify-content-between">
+                              
+                                <div class="days">
+                                    <i class="fa fa-clock-o"></i>
+                                    <a href="#">5 Days</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+                                @yield('footer')          <!-- extends the footer code here -->                                 
+</body>
+</html>
+
