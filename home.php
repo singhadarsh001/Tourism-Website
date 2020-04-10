53933ab57d4920b5f0a747df892d66195446ae06
singhadarsh001 <49779295+singhadarsh001@users.noreply.github.com>
2020-04-10 23:10:27 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,293 @@
+<?php
+
+  
+header('Location:header.php') ;
+
+?>
+
+
+<!doctype html>
+<html >
+<head>
+	<title> Home</title>
+	</head>
+
+<body>
+    <div class="slider">
+        <div class="slider_action">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-md-12">
+                            <div class="text-center">
+                                <h3>Patna</h3>
+                                <p>The capital city of Bihar, Patna serves as an important base for visiting various religious sites such as Vaishali, Bodh Gaya, Rajgir, Kesariya, Nalanda etc.</p>
+                                <a href="/patna" class="btn3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-md-12">
+                            <div class="text-center">
+                                <h3>Nalanda</h3>
+                                <p>Extensive ruins of center of higher learning in Bihar, Nalanda has various temples and monasteries, including the 'Great Stupa'.</p>
+                                <a href="/nalanda" class="btn3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-md-12">
+                            <div class="text-center">
+                                <h3>Bodhgaya</h3>
+                                <p>The world's most sacred Buddhist pilgrimage center, Bodh Gaya is believed to be the place where Gautam Buddha achieved Enlightenment.</p>
+                                <a href="/Bodhgaya" class="btn3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+    </div>
+<!-- About Bihar -->
+      <section >
+    
+        <div class="container">
+                    <div class="row align">
+                        <div class="col-xl-12 col-md-12">
+           <div ><br>
+<br><br>
+<br>
+      <h1 style="font-family:Nothing You Could Do, cursive; " 
+          style="font-size:60px">BIHAR </h1>
+        
+          <p style="font-size:20px">
+
+Bihar has remained an underrated tourist destination in 
+India. It is ironic that Bihar was once the seat of one of 
+the most prosperous ancient Indian kingdoms and today it 
+suffers listlessness when it comes to heritage tourism in 
+India. We agree, we may have a good amount of foreign 
+tourists swarming Bihar but said unsaid they have all 
+restricted themselves to fewer destinations in this 
+historically affluent state. If we see, the history of 
+Bihar is believed to have roots back to the breaking dawn 
+of civilizations in India and then the state prospered as 
+the seat of majestic empires like Magadh. It is also the 
+state that gave two important religions – Buddhism and 
+Jainism to the world. Bihar in course of years developed to 
+a rich historic site with diverse culture and tradition. 
+Today, what we witness is the flavoursome extracts of the 
+legacy of different empires. And if we make ourselves a 
+little more adjustable and open to travelling, we may be 
+able to explore an important gem from the Indian treasure 
+chest.
+Here is Bihar from the eye of an avid traveller and for the 
+sake of convenience the list has been split into three 
+sections, so that you do not leave any stone unturned. 
+Without any further ado, let us begin our journey into the 
+historic land of Bihar:
+</p>
+<br>
+<br>
+        </div>
+      </div>
+    </div>
+    </section>  
+    
+  <!-- About Bihar -->
+<!-- video play -->
+ <section >
+    
+        <div class="container">
+                    <div class="row align-center">
+                        <div class="col-xl-12 col-md-12">
+           <div >
+      <h1 style="font-family:Nothing You Could Do, cursive; " 
+          style="font-size:60px" align="center">BIHAR Tourism </h1>
+          </div>
+      </div>
+    </div>
+     </section> 
+
+ -->    <div class="video-overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <h3><br><br><br><br></h3>
+                        <video width="400" controls>
+  <source src="bihar.mp4" type="video/mp4">
+
+  Your browser does not have flash player.
+</video>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+ 
+     <br>
+     <br>
+     <br>
+<!-- video play end -->
+ <section >
+    
+        <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-md-12">
+           <div >
+      <h1 style="font-family:Nothing You Could Do, cursive; " 
+          style="font-size:60px" align="center">BIHAR OFFERS YOU </h1>
+          </div>
+      </div>
+    </div>
+     </section> 
+    <div class="travel_area">
+        <div class="container">
+            <div class="row">
+               
+                <div class="col-lg-4 col-md-6">
+                    <div class=" text-center">
+                        <div class="icon">
+                            <img src="img/icon/heritage.gif" alt="" height=250px width=300px>
+                        </div>
+                        <h3>Diverse Heritage</h3>
+                        <p>Bihar is full of cultural heritages all over its landscape moreover it has many modern marvels developed by mankind. </p>
+                    </div>
+                </div>
+                 <div class="col-lg-4 col-md-6">
+                    <div class="text-center">
+                        <div class="icon">
+                            <img src="img/icon/hotel.gif" alt="" height=250px width=300px>
+                        </div>
+                        <h3>5Star Hotels</h3>
+                        <p>With smart cities in most of the districts, Bihar offers you all the privelages for a comfortable and memorable trip.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="text-center">
+                        <div class="icon">
+                            <img src="img/icon/transport.gif" alt="" height=250px width=300px>
+                        </div>
+                        <h3>Comfortable Journey</h3>
+                        <p>With air and land services available at all tourist spots, we promise to make your tour comfortable and commendable.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Recent trips  -->
+   
+
+
+    <div class="trip_area">
+        <div class="container">
+            <div class="row justify">
+                <div class="col-lg-6">
+                    <div class="text-center mb_70">
+                        <h3>Types of Tours</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/ecotour.jpg" alt="" height=250px width=300px>
+                        </div>
+                        <div class="info">
+                           
+                            <a href="/eco">
+                                <h3>ECO Tourism</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/adventure.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            
+                            <a href="/adv">
+                                <h3>Adventure Tourism</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/heritage.jpg" alt="" height=250px width=300px>
+                        </div>
+                        <div class="info">
+                            
+                            <a href="/heritage">
+                                <h3>Heritage Tourism</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/modern.jpeg" alt="" height=250px width=300px>
+                        </div>
+                        <div class="info">
+                           
+                            <a href="/marvel">
+                                <h3>Modern Marvels</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/agritul.jpg" alt="" height=250px width=300px>
+                        </div>
+                        <div class="info">
+                          
+                            <a href="/agri">
+                                <h3>Agriculture Hotspots</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-lg-4 col-md-6">
+                    <div class="trip">
+                        <div class="thumb">
+                            <img src="img/trip/science.jpg" alt="" height=250px width=300px>
+                        </div>
+                        <div class="info">
+                           
+                            <a href="tech">
+                                <h3> Science and Technology</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php 
+
+include 'footer.php';
+
+?>
